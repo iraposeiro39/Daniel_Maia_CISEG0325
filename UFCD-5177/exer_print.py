3c34b2e3c58e11d437cecb47524c7871f182fbb4
Daniel Maia <daniel.maia.t0120684@edu.atec.pt>
2025-03-31 18:51:12 +0100

    Create exer_print.py

---
+++ UFCD-5177/exer_print.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+### Exercicio 1
+# Variaveis
+nome = "Joao"
+idade = 20
+cidade  = "Setubal"
+
+# Output
+print(f"Olá! O Meu nome é {nome}. Eu tenho {idade} e moro em {cidade}")
+
+### Exercicio 2
+# Output
+print("XXXXX\nX   X\nX   X\nXXXXX")
+
+### Exercicio 3
+# Output (print (one line))
+print("       X\n      XXX\n     XXXXX\n    XXXXXXX\n   XXXXXXXXX\n  XXXXXXXXXXX\n XXXXXXXXXXXXX\nXXXXXXXXXXXXXXX\n      XX\n      XX\n     XXXX")
+
+# Output (print (multi line))
+print("""
+       X
+      XXX
+     XXXXX
+    XXXXXXX
+   XXXXXXXXX
+  XXXXXXXXXXX
+ XXXXXXXXXXXXX
+XXXXXXXXXXXXXXX
+      XX
+      XX
+     XXXX
+""")
+
+# output (for)
+# for i in range(8):
--- UFCD-5177/input.py
+++ UFCD-5177/input.py
@@ -6,14 +6,21 @@
 # float -> 1,0 ... 9,0 ex: 3,1416
 # bool -> True OR False // 0 OR 1
 # str -> "Hello World! 9312919231"
+# casting ("cast") -> converte um tipo de dados em outro
 #
 # Variaveis
 num1 = 0
 num2 = 0
 total = 0
+
 # Inputs
 num1 = int(input("Numero 1: "))
 num2 = int(input("Numero 2: "))
+
 # Conta e output
-total = num1/num2
-print(f"A  divisão dos dois é: {total}")
+total = num1*num2
+print(f"A multiplicação dos dois é: {total}")
+
+# Print do tipo de variável
+print("tipo de variável")
+print(f"Numero1: {type(num1)} \nNumero2: {type(num2)}")
