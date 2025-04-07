762af18ecc598b91178e761b3cfe4062d1f45aef
Daniel Maia <daniel.maia.t0120684@edu.atec.pt>
2025-04-07 17:07:54 +0100

    Create exer11.py

---
+++ UFCD-5177/EXER_LOOP/exer11.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+# Exercício 11: Elabore um ciclo for para produzir o seguinte output.
+# 1
+# 22
+# 333
+num = "1"
+for i in range(1,4):
+    num_int = num * i
+    print(int(num_int) * i)
