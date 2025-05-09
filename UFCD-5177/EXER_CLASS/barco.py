7a15839e403326fc923ff75237be955d91ee45cf
Daniel Maia <daniel.maia.t0120684@edu.atec.pt>
2025-05-09 19:14:46 +0100

    Update barco.py

--- UFCD-5177/EXER_CLASS/barco.py
+++ UFCD-5177/EXER_CLASS/barco.py
@@ -3,16 +3,28 @@
 class Barcos:
     def __init__(self,nome,ano,carga,carga_max,velocidade):
         self.nome = nome
-        self.ano = ano
+        self.ano = 0
         self.carga = carga
         self.carga_max = carga_max
         self.velocidade = velocidade
 
-    def AumentarCarga(self,carga_adicional):
-        if self.carga + carga_adicional > self.carga_max:
-            print("Não é possivel adicionar mais carga! ")
-        else:
-            self.carga += carga_adicional
+    def getNome(self):
+        return self.nome
+
+    def getAno(self):
+        return self.ano
+
+    def getCarga(self):
+        return self.carga
+
+    def sumCarga(self,carga):
+        self.carga += carga
+
+    def getCarga_max(self):
+        return self.carga_max
+
+    def getVelocidade(self):
+        return self.velocidade
 
     def Acelerar(self,nivel):
         match nivel:
@@ -36,23 +48,28 @@ class Barcos:
 barco1 = Barcos("Zé Ferreira", 1976, 3600, 15000, 0)
 
 # Carga
-print(f"Carga do Barco {barco1.nome}: {barco1.carga}")
-barco1.AumentarCarga(10000)
-print(f"Carga do Barco {barco1.nome}: {barco1.carga}")
+print(f"Carga presente: {barco1.getCarga()}")
+print(f"Carga Máxima: {barco1.getCarga_max()}")
+num = int(input("Quanta carga é que pretendes adicionar ao barco?(kg): "))
+if barco1.getCarga() + num > barco1.getCarga_max():
+    print("Não é possivel adicionar tanta carga ao barco!")
+else:
+    barco1.sumCarga(num)
+print(f"Carga após adicionar: {barco1.getCarga()}")
 
 # Velocidade
-print(f"Velocidade do Barco {barco1.nome}: {barco1.velocidade}")
+print(f"Velocidade do Barco {barco1.getNome()}: {barco1.getVelocidade()}")
 barco1.Acelerar(2)
-print(f"Velocidade do Barco {barco1.nome}: {barco1.velocidade}")
+print(f"Velocidade do Barco {barco1.getNome()}: {barco1.getVelocidade()}")
 barco1.Travar(2)
-print(f"Velocidade do Barco {barco1.nome}: {barco1.velocidade}")
+print(f"Velocidade do Barco {barco1.getNome()}: {barco1.getVelocidade()}")
 barco1.Acelerar(3)
-print(f"Velocidade do Barco {barco1.nome}: {barco1.velocidade}")
+print(f"Velocidade do Barco {barco1.getNome()}: {barco1.getVelocidade()}")
 
 # Dados Finais do Barco
 print("== Barco nº1 ==")
-print(f"Nome: {barco1.nome}")
-print(f"Ano: {barco1.ano}")
-print(f"Carga Presente: {barco1.carga}")
-print(f"Carga Máxima: {barco1.carga_max}")
-print(f"Velocidade: {barco1.velocidade}")
+print(f"Nome: {barco1.getNome()}")
+print(f"Ano: {barco1.getAno()}")
+print(f"Carga Presente: {barco1.getCarga()}")
+print(f"Carga Máxima: {barco1.getCarga_max()}")
+print(f"Velocidade: {barco1.getVelocidade()}")
